f53135b57f28db4556344b67349b647d382fa116
Nic Martin <nicolas.martin@runpanther.io>
2022-08-23 13:21:49 -0500

    add package data for VERSION file to work

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     python_requires=">=3.9",
     long_description=long_description,
     long_description_content_type='text/markdown',
+    include_package_data=True,
     keywords='security detection',
     install_requires=[
         'panther_core>=0.0.6,<0.1.0',
